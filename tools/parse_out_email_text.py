2bf3ac122230fc3494da77072f852d571ebc8146
bnareyn <binulal@gmail.com>
2019-09-26 19:02:14 +0530

    Update parse_out_email_text.py
    added stem logic

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,16 +28,13 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
-
+        #words = text_string
+        stemmer = stemmer = SnowballStemmer("english")
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
-        ### space between each stemmed word)
-        
-
-
-
-
+        ### space between each stemmed words
+        stem_words = [stemmer.stem(word) for word in text_string.split()]
+        words = ' '.join(stem_words)
     return words
 
     
